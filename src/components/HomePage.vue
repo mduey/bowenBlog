a5c267ca0c0c07ab051d020e98b0799e302fa44a
Michael Duey <michaelduey@gmail.com>
2024-01-16 19:40:11 -0800

    misc changes

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -14,10 +14,17 @@ function test() {
   <h1 style="margin-bottom: 4rem">Duey.org</h1>
 
   <div class="post">asfdasdfasdfasdfsadfasdf</div>
+
+  <div class="post">
+    adfgererg789789789789789789789879766575 546456 456456456456456546 546 546
+    546456456456456456456546546546546456asdfsadfasdf
+  </div>
 </template>
 
 <style scoped>
 .post {
   color: red;
+  border: 1px solid gray;
+  margin-bottom: 3px;
 }
 </style>
